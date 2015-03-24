372175a44d903b402ba665c7fa197f56fba68e22
Jardel Anton <jardellanton@gmail.com>
2015-03-24 16:40:14 -0300

    Interpretador

---
+++ Interpretador.java
@@ -0,0 +1,12 @@
+class Interpretador{
+	public static void main(String args[]){
+		Variavel[] vetor;
+		vetor[0] = new Variavel();
+		public Variavel getVariavel(String nome){
+			for(int i=0;i<vetor.size();i++){
+				System.out.println(vetor.size());
+			}
+		}
+
+	}	
+}
