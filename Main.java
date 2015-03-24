ef20693db159f33c0226fd32448e5572eb9fa98a
Jardel Anton <jardellanton@gmail.com>
2015-03-24 18:13:44 -0300

    testes realizados (Main)

--- Main.java
+++ Main.java
@@ -12,11 +12,31 @@ class Main{
 		a=var.getValor();
 		b=var.getNome();
 		System.out.println(b +" "+a);
+
 		inter.criaVariavel(var);
-		a=inter.vetor[0].getValor();
-		b=inter.vetor[0].getNome();
-		System.out.println(a +" "+b);
-		
+
+		var.setNome("Lisle");
+		var.setValor(16.0);
+		inter.criaVariavel(var);
+
+		var.setNome("Luany");
+		var.setValor(5.0);
+		inter.criaVariavel(var);
+
+		var.setNome("Lara");
+		var.setValor(3.0);
+		inter.criaVariavel(var);
+
+		var=inter.getVariavel("Lisle");
+		System.out.println(var.getNome()+" "+var.getValor());
+		var=inter.getVariavel("Jardel");
+		System.out.println(var.getNome()+" "+var.getValor());
+		var=inter.getVariavel("Luany");
+		System.out.println(var.getNome()+" "+var.getValor());
+		var=inter.getVariavel("Lisle");
+		System.out.println(var.getNome()+" "+var.getValor());
+		var=inter.getVariavel("Lara");
+		System.out.println(var.getNome()+" "+var.getValor());
 	}
 
 }
